5945329a3d1955685b72c65aa3293c8e995ebe5f
= <=>
2016-11-24 17:09:26 +0800

    DS作业

---
+++ DS/DS_Assignments/RMI/src/main/java/com/sunzequn/ds/rmi/MainApp.java
@@ -0,0 +1,11 @@
+package com.sunzequn.ds.rmi;
+
+/**
+ * Created by Sloriac on 2016/11/24.
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
